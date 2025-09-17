270682893cc6c4b899c28aff1b883eaa368156c3
chetan-911 <suryawanshichetan290@gmail.com>
2025-09-17 23:33:40 +0530

    Initial commit

---
+++ dist/advanced-query.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=advanced-query.d.ts.map
---
+++ dist/create-post.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+async function main() {
+    await prisma.post.create({
+        data: {
+            title: "title of post",
+            content: "Book",
+            author: {
+                connect: {
+                    id: 1
+                }
+            }
+        }
+    });
+}
+main();
+//# sourceMappingURL=create-post.js.map
